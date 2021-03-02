5e21d8b5817380346074e4f35e3d52913108268b
Miroslav Stefanović <mstef@uns.ac.rs>
2021-03-02 12:55:37 +0100

    Vežbe 2. termin

---
+++ rpp2021-vezbe/src/test/java/rpp2021vezbe/Rpp2021VezbeApplicationTests.java
@@ -0,0 +1,13 @@
+package rpp2021vezbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rpp2021VezbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
